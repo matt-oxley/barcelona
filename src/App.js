942a55ca9e3d0e222d8925aecbbe315f7a45fc86
Matthew Oxley <hello@mattoxley.me>
2019-08-19 16:56:18 +0200

    more performant resize listener

--- src/App.js
+++ src/App.js
@@ -1,38 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import Map from "./Map";
 import Console from "./Console";
 import styles from "./App.module.css";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       currentBarrio: null
-    }
-    this.setCurrentBarrio = this.setCurrentBarrio.bind(this)
+    };
+    this.setCurrentBarrio = this.setCurrentBarrio.bind(this);
   }
   setCurrentBarrio(key) {
-    console.log('hi', key)
     this.setState({
       currentBarrio: key
-    })
+    });
   }
-  componentDidMount() {
-    this.resizeListener();
-    window.addEventListener("resize", this.resizeListener);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.resizeListener);
-  }
-  resizeListener = () => {
-    this.forceUpdate();
-  };
+
   render() {
-    console.log('yo', this.state.currentBarrio)
     return (
       <div className={styles.wrap}>
-        <Map setCurrentBarrio={this.setCurrentBarrio}/>
-        <Console currentBarrio={this.state.currentBarrio}/>
+        <Map setCurrentBarrio={this.setCurrentBarrio} />
+        <Console currentBarrio={this.state.currentBarrio} />
       </div>
     );
   }
--- src/useD3.js
+++ src/useD3.js
@@ -3,6 +3,13 @@ import { select } from "d3";
 
 export default function(cb) {
   const ref = useRef(null);
-  useEffect(() => cb(select(ref.current)));
+  useEffect(() => {
+    function paintD3() {
+      cb(select(ref.current));
+    }
+    paintD3();
+    window.addEventListener("resize", paintD3);
+    return () => window.removeEventListener("resize", paintD3);
+  }, [cb]);
   return ref;
 }
